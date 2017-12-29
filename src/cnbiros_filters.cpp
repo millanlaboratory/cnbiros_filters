a9c1a5326115fc6720c2ab8e6a9c74cd5803bcd5
Luca Tonin <luca.tonin@epfl.ch>
2017-12-29 18:14:44 +0100

    Renamed plugin implementation

--- src/cnbiros_filters.cpp
+++ src/cnbiros_filters.cpp
@@ -1,8 +1,9 @@
-#ifndef CNBIROS_FILTERS_RANGE_FILTERS_CPP
-#define CNBIROS_FILTERS_RANGE_FILTERS_CPP
+#ifndef CNBIROS_FILTERS_CPP
+#define CNBIROS_FILTERS_CPP
 
 #include "cnbiros_filters/RangeThresholdFilter.hpp"
 #include <pluginlib/class_list_macros.h>
+
 
 PLUGINLIB_EXPORT_CLASS(cnbiros::range_filters::RangeThresholdFilter, filters::FilterBase<sensor_msgs::Range>)
 
